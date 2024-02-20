513db08ab3117a90f8bdf38c616570852033a298
lahssiki <anaslahssiki@gmail.com>
2024-02-20 15:52:05 +0100

    add commande

--- src/Commande/CommandeList.jsx
+++ src/Commande/CommandeList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Form, Button, Table } from 'react-bootstrap';
+import { Form, Button, Table, Modal } from 'react-bootstrap';
 import Navigation from "../nav/Navigation";
 
 const CommandeList = () => {
@@ -8,6 +8,9 @@ const CommandeList = () => {
   const [selectedClient, setSelectedClient] = useState('');
   const [produits, setProduits] = useState([]);
   const [selectedProducts, setSelectedProducts] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     fetchClients();
@@ -21,7 +24,7 @@ const CommandeList = () => {
   const fetchClients = async () => {
     try {
       const response = await axios.get("http://localhost:8000/api/clients");
-      setClients(response.data.client); // Utiliser response.data.client au lieu de response.data.client
+      setClients(response.data.client);
     } catch (error) {
       console.error("Error fetching clients:", error);
     }
@@ -36,19 +39,32 @@ const CommandeList = () => {
     }
   };
 
-  const handleProductSelect = (id) => {
-    console.log("Selected Product ID:", id); // Ajoutez cette ligne pour vérifier la valeur sélectionnée
-    const selectedProduct = produits.find(produit => produit.id === id);
-    setSelectedProducts([...selectedProducts, selectedProduct]);
-    
-    console.log("Selected Products:", selectedProducts);
+  const handleProductSelect = (id, isChecked) => {
+    const product = produits.find(produit => produit.id === id);
+    if (isChecked) {
+      setSelectedProduct(product);
+      setShowModal(true);
+    } else {
+      setSelectedProduct(null);
+    }
+  };
+
+  const handleAddProduct = () => {
+    const productToAdd = { ...selectedProduct, quantity };
+    setSelectedProducts([...selectedProducts, productToAdd]);
+    setShowModal(false);
+  };
+
+  const handleRemoveProduct = (id) => {
+    const updatedProducts = selectedProducts.filter(product => product.id !== id);
+    setSelectedProducts(updatedProducts);
   };
 
   return (
     <div className="col row" >
       <Navigation />
-      <div className="col-2 mt-2 ">
-      <Form.Label className="mt-2 m-2"><a  href="/clients">LISTE DES CLIENTS</a>:</Form.Label>
+      <div className="col-2 mt-3">
+        <Form.Label><a className=" m-3" href="/clients">Liste des Clients</a></Form.Label>
         <Form.Select className="col-4 m-2 mt-2" value={selectedClient} onChange={handleClientChange}>
           <option value="">Sélectionner un client</option>
           {clients.map((client) => (
@@ -59,20 +75,42 @@ const CommandeList = () => {
         </Form.Select>
       </div>
 
-      <div className="col-3 mt-3">
-        <Form.Group controlId="productSelect">
-          <Form.Label>Sélectionner un produit:</Form.Label>
-          <Form.Control as="select" onChange={(e) => handleProductSelect(e.target.value)}>
-            <option value="">Choisir un produit</option>
+      <div className="col-4 mt-3">
+      <Form.Label>Sélectionner des produits:</Form.Label>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nom</th>
+              <th>Type Quantité</th>
+              <th>Calibre</th>
+              <th>Add</th>
+            </tr>
+          </thead>
+          <tbody>
             {produits.map(produit => (
-              <option key={produit.id} value={produit.id}>{produit.nom}</option>
+              <tr key={produit.id}>
+                <td>{produit.id}</td>
+                <td>{produit.nom}</td>
+                <td>{produit.type_quantite}</td>
+                <td>{produit.calibre}</td>
+
+                <td>
+                  <Form.Check
+                    type="checkbox"
+                    id={`produit-${produit.id}`}
+                    onChange={(e) => handleProductSelect(produit.id, e.target.checked)}
+                  />
+                </td>
+              </tr>
             ))}
-          </Form.Control>
-        </Form.Group>
+          </tbody>
+        </Table>
+
       </div>
 
-      <div className="container mt-3 col-6" >
-        <Form.Label>Produits Sélectionnés</Form.Label>
+      <div className="container mt-3 col-5" >
+        <h4>Produits Sélectionnés</h4>
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -83,21 +121,37 @@ const CommandeList = () => {
             </tr>
           </thead>
           <tbody>
-            {selectedProducts && selectedProducts.map(produit => (
-              produit && (
-                <tr key={produit.id}>
-                  <td>{produit.nom}</td>
-                  <td>{produit.type_quantite}</td>
-                  <td>{produit.calibre}</td>
-                  <td>
-                    <Button variant="danger">Supprimer</Button>
-                  </td>
-                </tr>
-              )
+            {selectedProducts.map(produit => (
+              <tr key={produit.id}>
+                <td>{produit.nom}</td>
+                <td>{produit.type_quantite}</td>
+                <td>{produit.calibre}</td>
+                <td>
+                  <Button variant="danger" onClick={() => handleRemoveProduct(produit.id)}>Supprimer</Button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </Table>
       </div>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sélectionner la quantité</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Label>Quantité:</Form.Label>
+          <Form.Control
+            type="number"
+            value={quantity}
+            onChange={(e) => setQuantity(parseInt(e.target.value))}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>Annuler</Button>
+          <Button variant="primary" onClick={handleAddProduct}>Ajouter</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
